751531cb4c37183bf3999e6a97604cd38147f95a
GerardoAmaya <00174220@uca.edu.sv>
2023-03-20 09:03:04 -0600

    php register and login laravel ui

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,14 @@
 
 use App\Http\Controllers\StudentController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Auth::routes();
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::get('students', [StudentController::class, 'index']);
 Route::post('students', [StudentController::class, 'store']);
@@ -10,3 +17,5 @@ Route::get('fetch-students', [StudentController::class, 'fetchstudent']);
 Route::get('edit-student/{id}', [StudentController::class, 'edit']);
 Route::put('update-student/{id}', [StudentController::class, 'update']);
 Route::delete('delete-student/{id}', [StudentController::class, 'destroy']);
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
